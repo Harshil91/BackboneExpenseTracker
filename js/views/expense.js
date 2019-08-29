bee7d7a4e1a56e71a0d64b0ef6478e36bc11c2d0
Harshil J <harshiljasra@gmail.com>
2019-08-29 10:08:39 -0400

    Remove expense from view

--- js/views/expense.js
+++ js/views/expense.js
@@ -2,6 +2,10 @@ const ExpenseView = Backbone.View.extend({
   tagName: 'div',
   className: 'expense',
 
+  events: {
+    'click.delete': 'removeExpense'
+  },
+
   template: _.template(`
     <div class="field">
       <h2><%= description %></h2>
@@ -12,12 +16,21 @@ const ExpenseView = Backbone.View.extend({
     <div class="field">
       <h2><%= amount %></h2>
     </div>    
+
+    <div class="actions">
+      <button class="delete">Delete</button>
+
+    </div>
   `),
 
   render(){
     const model = this.model.toJSON();
     this.el.innerHTML = this.template(model);
     return this;
+  },
+
+  removeExpense(){
+    this.remove();
   }
 
 });
