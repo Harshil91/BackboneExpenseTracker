39b20cfb6fa79a880ffcf02a3da9ef0104980b59
Harshil J <harshiljasra@gmail.com>
2019-08-29 09:51:49 -0400

    localStorage initial setup to save expense

--- js/collection/expenses.js
+++ js/collection/expenses.js
@@ -1,4 +1,15 @@
 const Expenses = Backbone.Collection.extend({
   model: Expense,
 
+
+  initialize(){
+    this.on('add', this.addToDB);
+  },
+
+  addToDB(expense){
+    const id = expense.get('id');
+
+    localStorage.setItem(id,JSON.stringify(expense));
+  }
+
 });
