9d855052a707b86310df5563861989363aa1d3ac
Harshil J <harshiljasra@gmail.com>
2019-08-31 15:54:54 -0400

    Setup for sorting

--- js/views/expenses.js
+++ js/views/expenses.js
@@ -2,12 +2,34 @@ const ExpensesView = Backbone.View.extend({
   tagName: 'div',
   id: 'expenses',
 
+  ASC_SYMBOL: '&#9660;', 
+  DESC_SYMBOL: '&#9650;',
+
+  template: _.template(`
+    <div id="sort-actions">
+      Sort: 
+      <a href="#/sort/date/<%= dateDirection %>" id="sort-date">
+        Date <%= dateSymbol %>
+      </a>
+      <a href="#/sort/amount/<%= amountDirection %>" id="sort-amount">
+        Amount <%= amountSymbol %>
+      </a>
+    </div>
+  `),
+
   initialize(){
     this.listenTo(this.collection, 'add', this.render);
   },
   
   render(){
-    this.el.innerHTML = '';
+    this.el.innerHTML = this.template({
+      dateDirection: 'asc',
+      amountDirection: 'asc',
+
+      amountSymbol: this.ASC_SYMBOL,
+      dateSymbol: '',
+
+    });
     this.collection.forEach((expense) => {
       this.addExpense(expense);
     });
